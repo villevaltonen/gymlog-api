9b0288023782a44fb71191013fffe7d30365babe
Ville Valtonen <valtonen.vt@gmail.com>
2020-12-20 20:09:45 +0200

    - jwt auth initial commit

--- app.go
+++ app.go
@@ -4,11 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	"encoding/json"
 	"net/http"
 	"strconv"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
@@ -173,4 +175,186 @@ func (a *Application) initializeRoutes() {
 	a.Router.HandleFunc("/api/v1/sets/{id:[0-9]+}", a.getSet).Methods("GET")
 	a.Router.HandleFunc("/api/v1/sets/{id:[0-9]+}", a.updateSet).Methods("PUT")
 	a.Router.HandleFunc("/api/v1/sets/{id:[0-9]+}", a.deleteSet).Methods("DELETE")
+	a.Router.HandleFunc("/api/login", Login).Methods("POST")
+	a.Router.HandleFunc("/api/welcome", Welcome).Methods("GET")
+	a.Router.HandleFunc("/api/refresh", Refresh).Methods("POST")
+}
+
+// jwt
+
+// switch to env variable
+var jwtKey = []byte("my_secret_key")
+
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+// Struct for username and password
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+// Struct, which will be encoded to JWT
+// Includes embedded type jwt.StandardClaims to provide additional fields like expiry time
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+// Handler for login
+func Login(w http.ResponseWriter, r *http.Request) {
+	var creds Credentials
+
+	// Decode JSON body to get credentials
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		// invalid structure results to HTTP error
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Get expected password from our in memory map
+	// TODO: Use database
+	expectedPassword, ok := users[creds.Username]
+
+	// If password exists for the given user
+	// AND, if it is the same as in request body, we can move ahead
+	// if NOT, return unauthorized status
+	if !ok || expectedPassword != creds.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Declare expiration time for token
+	expirationTime := time.Now().Add(5 * time.Minute)
+	// Create JWT claims, which include username and expiration time
+	claims := &Claims{
+		Username: creds.Username,
+		StandardClaims: jwt.StandardClaims{
+			// in JWT, expiration time is given as unix milliseconds
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	// Declare the token with algorithm used for signing and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create JWT string
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		// In case of error, return internal server error
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Finally, we set the client cookie for token and use the same expiration time
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	// We can obtain the session token from auth requests cookies
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			// If cookie is not set, set unauthorized status
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		// For any other type of request return bad request
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	// Get JWT string from token
+	tknStr := c.Value
+
+	// Initialize a new instance of claims
+	claims := &Claims{}
+
+	// Parse the JWT string and store the result in `claims`.
+	// Note that we are passing the key in this method as well. This method will return an error
+	// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+	// or if the signature does not match
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Finally return welcome to the user along with the username
+	w.Write([]byte(fmt.Sprintf("Welcome %s!", claims.Username)))
+}
+
+func Refresh(w http.ResponseWriter, r *http.Request) {
+	// (BEGIN) The code uptil this point is the same as the first part of the `Welcome` route
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			fmt.Println("No cookie present")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	tknStr := c.Value
+	claims := &Claims{}
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			fmt.Println("Err sign invalid")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		fmt.Println("token invalid")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	// (END) The code up-till this point is the same as the first part of the `Welcome` route
+
+	// We ensure that a new token is not issued until enough time has elapsed
+	// In this case, a new token will only be issued if the old token is within
+	// 30 seconds of expiry. Otherwise, return a bad request status
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Now, create a new token for the current use, with a renewed expiration time
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims.ExpiresAt = expirationTime.Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Set the new token as the users `token` cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/villevaltonen/gymlog-go
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gorilla/mux v1.8.0
 	github.com/lib/pq v1.9.0
 )
